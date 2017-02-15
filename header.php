32c8fc8729532edbe78a6a362141c2cdeca1467a
tomipohj <tomipohj@metropolia.fi>
2017-02-15 14:03:12 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -18,30 +18,27 @@
     
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <nav class="navbar navbar-light bg-faded">
         <div class="container">
-            <a class="navbar-brand" href="http://users.metropolia.fi/~aleksr/wp_projekti/">Header</a>
+            <div class="row">
+            <div class="col-4">
+                <a class="navbar-brand" href="http://users.metropolia.fi/~aleksr/wp_projekti/">Logo  <span class="fa fa-home"></span></a>
+                
+            </div>
+            <div class="col-4">
+                <h5 class="slogan">'quality over quantity'</h5>
+            </div>
+            <div class="col-4">
+                <a class="navbar-brand checkout" href="http://users.metropolia.fi/~aleksr/wp_projekti/checkout"><span class="fa fa-shopping-cart checkouticon"></span></a>
+                
+                <a class="navbar-brand checkout" href="http://users.metropolia.fi/~aleksr/wp_projekti/checkout">Checkout</a>
+                
+            </div>
+            </div>
         </div>
     </nav>
-    
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-                <img class="d-block img-fluid" src="https://trello-attachments.s3.amazonaws.com/587dd2f3c4ef0b80fbe11bf5/589d7ebfcf3d25371215c25f/d920fbc1e09a0ab3c31acbcde1a63697/carousel1.jpg" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block img-fluid" src="https://trello-attachments.s3.amazonaws.com/587dd2f3c4ef0b80fbe11bf5/589d7eef3a9111557ce708d8/3c35e61ccd27e6879fa931e1be4df035/carousel2.jpg" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block img-fluid" src="https://trello-attachments.s3.amazonaws.com/587dd2f3c4ef0b80fbe11bf5/589d7eea61fb6b71bdfeea6a/5cedb1fd9f085f1a78e7abc6f3223de1/carousel3.jpg" alt="Third slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block img-fluid" src="https://trello-attachments.s3.amazonaws.com/587dd2f3c4ef0b80fbe11bf5/589d7f78ffe06ef9f643fe21/8f1991bfb2e85713777059cfd158e0be/carousel4.jpg" alt="Third slide">
-            </div>
-        </div>
-    </div>
-    
-    <div class="container">
